d5234e721c6b3a5c884b79a7522c74fa673e2afb
saifyahya <saifbarakat991@gmail.com>
2023-10-25 20:33:20 +0000

    room

--- app/src/main/java/com/demo/myfirstapplication/activity/TaskDetailsActivity.java
+++ app/src/main/java/com/demo/myfirstapplication/activity/TaskDetailsActivity.java
@@ -4,33 +4,74 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.demo.myfirstapplication.R;
+import com.demo.myfirstapplication.activity.database.DatabaseSingleton;
+import com.demo.myfirstapplication.activity.database.TaskDatabase;
+import com.demo.myfirstapplication.activity.enums.TaskState;
+import com.google.android.material.snackbar.Snackbar;
 
 public class TaskDetailsActivity extends AppCompatActivity {
     TextView taskTitle;
     String taskTitleStr;
+    TextView taskBody;
+    String taskBodyStr;
+    TextView taskEndDate;
+    String taskEndDateStr;
+    TaskDatabase taskDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_details);
+        /*Displaying Task Info*/
+        Intent callingIntent = getIntent();
+        if (callingIntent != null){
+            taskTitleStr = callingIntent.getStringExtra(MainActivity.TASK_TAG);
+            taskBodyStr=callingIntent.getStringExtra("taskBody");
+            taskEndDateStr = callingIntent.getStringExtra("taskDate");
+        }
+        taskTitle = findViewById(R.id.textViewTitle);
+        settingTaskInfo(taskTitle,taskTitleStr);
+
+        taskBody = findViewById(R.id.taskDetailsDescription);
+        settingTaskInfo(taskBody,taskBodyStr);
+
+        taskEndDate = findViewById(R.id.taskDateView);
+        settingTaskInfo(taskEndDate,taskEndDateStr);
+
+        /*Database connection*/
+taskDatabase= DatabaseSingleton.getInstance(getApplicationContext());
+
+        /*updating Task State Part*/
+        Spinner taskState = findViewById(R.id.spinner);
+        taskState.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,
+                TaskState.values()));
+        Button changeState = findViewById(R.id.changeStateButton);
+        changeState.setOnClickListener(view -> {
+           long retrievedTaskId = callingIntent.getLongExtra("taskId",-1);
+            TaskState newTaskState = TaskState.fromString(taskState.getSelectedItem().toString());
+            if(retrievedTaskId!=-1)
+           taskDatabase.taskDAO().updateTaskState(newTaskState,retrievedTaskId);
+            Snackbar.make(findViewById(R.id.TaskDetailsLayout), "Task state updated", Snackbar.LENGTH_SHORT).show();
+        });
 
         ImageView back = findViewById(R.id.backbutton);
         back.setOnClickListener(view ->  {
             Intent gobackFormIntent = new Intent(TaskDetailsActivity.this, MainActivity.class);
             startActivity(gobackFormIntent);
         });
-
-        Intent callingIntent = getIntent();
-        if (callingIntent != null)
-            taskTitleStr = callingIntent.getStringExtra(MainActivity.TASK_TAG);
-        taskTitle = findViewById(R.id.textViewTitle);
-        if ((taskTitleStr != null))
-            taskTitle.setText(taskTitleStr);
+    }
+    private void settingTaskInfo(TextView textView , String textViewStr){
+        if ((textViewStr != null))
+            textView.setText(textViewStr);
         else
-            taskTitle.setText("Not Specified");
+            textView.setText("Not Specified");
     }
 }
--- app/src/main/java/com/demo/myfirstapplication/activity/models/Task.java
+++ app/src/main/java/com/demo/myfirstapplication/activity/models/Task.java
@@ -1,16 +1,38 @@
 package com.demo.myfirstapplication.activity.models;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.demo.myfirstapplication.activity.enums.TaskState;
 
+import java.util.Date;
+
+@Entity
 public class Task {
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
     private String title;
     private String body;
     private TaskState state;
+    private Date endDate;
 
-    public Task(String title, String body, TaskState state) {
+    public Task(String title, String body, TaskState state,Date endDate) {
         this.title = title;
         this.body = body;
         this.state = state;
+        this.endDate = endDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitle() {
